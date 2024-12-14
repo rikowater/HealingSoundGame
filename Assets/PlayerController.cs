f08c04ec99b654ceed1019de9b1307372baec16b
rikowater <132248895+rikowater@users.noreply.github.com>
2024-12-14 21:48:56 +0900

    1214_1

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,51 +4,63 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
+    private Transform _transform;
+    private Animator _animator;
+    private float _horizontal;
+    private float _vertical;
+    private Vector3 _velocity;
+    private float _speed = 2f;
 
-private Rigidbody _rigidbody;
-private Transform _transform;
-private Animator _animator;
-private float _horizontal;
-private float _vertical;
-private Vector3 _velocity;
-private float _speed = 2f;
+    private Vector3 _aim; // 進行方向
+    private Quaternion _playerRotation; // キャラクターの回転
 
-private Vector3 _aim; // 追記
-private Quaternion _playerRotation; // 追記
+    public FixedJoystick inputMove; // JoyStick
 
-void Start()
-{
-_rigidbody = GetComponent<Rigidbody>();
-_transform = GetComponent<Transform>();
-_animator = GetComponent<Animator>();
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
+        _animator = GetComponent<Animator>();
 
-_playerRotation = _transform.rotation; // 追記
+        _playerRotation = _transform.rotation;
+    }
 
-}
+    void FixedUpdate()
+    {
+        // ジョイスティックの入力を取得
+        _horizontal = inputMove.Horizontal;
+        _vertical = inputMove.Vertical;
 
-void FixedUpdate()
-{
-_horizontal = Input.GetAxis("Horizontal");
-_vertical = Input.GetAxis("Vertical");
+        // カメラの向きを基準にした回転を計算
+        var _horizontalRotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
 
-var _horizontalRotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up); // 追記
+        // 入力方向をカメラ基準で計算
+        _velocity = _horizontalRotation * new Vector3(_horizontal, 0, _vertical).normalized;
 
-_velocity = _horizontalRotation * new Vector3(_horizontal, _rigidbody.velocity.y, _vertical).normalized; // 修正
+        // 進行方向を計算
+        _aim = _velocity;
 
-_aim = _horizontalRotation * new Vector3(_horizontal, 0, _vertical).normalized; // 追記
+        // 移動中のキャラクターの向き設定
+        if (_aim.magnitude > 0.1f)
+        {
+            _playerRotation = Quaternion.LookRotation(_aim, Vector3.up);
+        }
 
-if(_aim.magnitude > 0.5f) { // 以下追記
-_playerRotation = Quaternion.LookRotation(_aim, Vector3.up);
-}
+        // キャラクターの回転をスムーズに反映
+        _transform.rotation = Quaternion.RotateTowards(_transform.rotation, _playerRotation, 600 * Time.deltaTime);
 
-_transform.rotation = Quaternion.RotateTowards(_transform.rotation, _playerRotation, 600 * Time.deltaTime); // 追記
+        // アニメーションの設定
+        if (_velocity.magnitude > 0.1f)
+        {
+            _animator.SetBool("Walk", true);
+        }
+        else
+        {
+            _animator.SetBool("Walk", false);
+        }
 
-if (_velocity.magnitude > 0.1f) {
-_animator.SetBool("Walk", true);
-} else {
-_animator.SetBool("Walk", false);
-}
-
-_rigidbody.velocity = _velocity * _speed;
-}
+        // Rigidbodyを使った移動処理
+        _rigidbody.velocity = _velocity * _speed;
+    }
 }
