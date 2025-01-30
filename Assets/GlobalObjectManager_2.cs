4015df2d38045d41ebd75cba1a96b643763ac038
rikowater <132248895+rikowater@users.noreply.github.com>
2025-01-31 03:36:07 +0900

    0131

---
+++ Assets/GlobalObjectManager_2.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlobalObjectManager_2 : MonoBehaviour
+{
+    // シーン間で共有するオブジェクトの参照
+    public GameObject sharedObject;  // ← これが定義されていることを確認
+
+    [SerializeField] private List<string> activeScenes;
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        CheckSceneActive();
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        CheckSceneActive();
+    }
+
+    private void CheckSceneActive()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+        if (sharedObject != null)
+        {
+            sharedObject.SetActive(activeScenes.Contains(currentScene));
+        }
+    }
+
+}
